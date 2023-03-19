4ee3e445a0aa597c5c17832032b8ea582679b509
Adson Nascimento <adson.nascimento.112@ufrn.edu.br>
2023-03-19 12:04:00 -0300

    questions on

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <template v-if="this.question">
+        <h1 v-html="this.question"></h1>
+
+      <template v-for="answar in this.answars" :key="answar">
+
+        <input type="radio" name="options" value="answar">
+        <label v-html="answar"></label><br>
+
+      </template>
+    </template>
+
+    <button class="send" type="button">Send</button>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+
+    data() {
+      return {
+        question: undefined,
+        incorrectAnswars: undefined,
+        correctAnswars: undefined
+      }
+    },
+
+    computed: {
+      answars() {
+        var answars = JSON.parse( JSON.stringify( this.incorrectAnswars ) );
+        answars.splice( Math.round(Math.random() * answars.lenght ), 0, this.correctAnswars );
+
+        return answars;
+      }
+    },
+
+    created() {
+      this.axios
+        .get("https://opentdb.com/api.php?amount=1&type=multiple")
+        .then((response) => {
+          this.question = response.data.results[0].question;
+          this.incorrectAnswars = response.data.results[0].incorrect_answers;
+          this.correctAnswars = response.data.results[0].correct_answer;
+        })
+    }
   }
-}
+
 </script>
 
 <style lang="scss">
@@ -21,6 +59,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 900px;
+
+  input[type=radio] {
+    margin: 12px 4px
+  }
+
+  button.send {
+    margin-top: 12px;
+    min-width: 120px;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    color: #fff;
+    cursor: pointer;
+    text-align: center;
+    height: 40px;
+  }
 }
 </style>
