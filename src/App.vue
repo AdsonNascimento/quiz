fb6eb62ba29c5274be3fa51ed2dd2ed257a894c5
Adson Nascimento <adson.nascimento.112@ufrn.edu.br>
2023-03-20 21:53:30 -0300

    terminal

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,112 @@
 <template>
 
   <div>
+
+    <ScoreBoard :winCount="this.winCount" :loseCount="this.loseCount" />
+
     <template v-if="this.question">
         <h1 v-html="this.question"></h1>
 
-      <template v-for="answar in this.answars" :key="answar">
+      <template v-for="(answar, index) in this.answars" :key="index">
 
-        <input type="radio" name="options" value="answar">
-        <label v-html="answar"></label><br>
+        <input 
+          :id="index" 
+          :value="answar"
+          :disabled="this.answarSubimitted" 
+          type="radio" 
+          name="options" 
+          v-model="this.chosenAnswer"
+        >
+        <label :for="index" v-html="answar"></label><br>
 
       </template>
+
+      <section v-if="this.answarSubimitted">
+        <h4 v-if="this.correctAnswar == this.chosenAnswer">
+          &#9989; você acertou! Parabéns
+        </h4>
+
+        <h4 
+          v-else
+          v-html="'&#10060; Opa! você errou... A resposta correta é ' + this.correctAnswar"
+        >
+        </h4>
+
+        <button @click="this.getNewQuestion()" class="send" type="button">Proxima questão</button>
+      </section>
+
+      <button v-if="!this.answarSubimitted" @click="submitAnswer()" class="send" type="button">enviar</button>
+
     </template>
 
-    <button class="send" type="button">enviar</button>
   </div>
 
 </template>
 
 <script>
+  import ScoreBoard from "./components/ScoreBoard.vue";
 
   export default {
+
     name: 'App',
+    components: {
+      ScoreBoard
+    },
 
     data() {
       return {
         question: undefined,
-        incorrectAnswars: undefined,
-        correctAnswars: undefined
+        incorrectAnswar: undefined,
+        correctAnswar: undefined,
+        chosenAnswer: undefined,
+        answarSubimitted: false,
+        winCount: 0,
+        loseCount: 0
       }
     },
 
     computed: {
       answars() {
-        var answars = JSON.parse( JSON.stringify( this.incorrectAnswars ) );
-        answars.splice( Math.round(Math.random() * answars.lenght ), 0, this.correctAnswars );
+        var answars = JSON.parse( JSON.stringify( this.incorrectAnswar ) );
+        answars.splice( Math.round( Math.random() * 4 ), 0, this.correctAnswar );
 
         return answars;
       }
     },
 
+    methods: {
+      submitAnswer() {
+        if(!this.chosenAnswer) {
+          alert('Escolha uma opção!');
+        } else {
+
+          this.answarSubimitted = !this.answarSubimitted;
+
+          if (this.chosenAnswer == this.correctAnswar) {
+            this.winCount++;
+          } else {
+            this.loseCount++;
+          }
+        }
+      },
+      getNewQuestion() {
+
+        this.answarSubimitted = false;
+        this.chosenAnswer = undefined;
+        this.question = undefined;
+
+        this.axios
+          .get("https://opentdb.com/api.php?amount=1&category=10&difficulty=easy&type=boolean")
+          .then((response) => {
+            this.question = response.data.results[0].question;
+            this.incorrectAnswar = response.data.results[0].incorrect_answers;
+            this.correctAnswar = response.data.results[0].correct_answer;
+          })
+      }
+    },
+
     created() {
-      this.axios
-        .get("https://opentdb.com/api.php?amount=1&type=multiple")
-        .then((response) => {
-          this.question = response.data.results[0].question;
-          this.incorrectAnswars = response.data.results[0].incorrect_answers;
-          this.correctAnswars = response.data.results[0].correct_answer;
-        })
+      this.getNewQuestion()
     }
   }
 
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="score">
+        <p>Player <span>{{ this.winCount }}</span> x <span>{{ this.loseCount }}</span> Computer</p>
+    </section>
+</template>
+
+<script>
+    export default {
+        props: [ "winCount", "loseCount" ]
+    }
+</script>
+
+<style lang="scss">
+section.score {
+    border-bottom: 1px solid black;
+    padding: 12px;
+    font-size: 18px;
+
+    span {
+        padding: 8px;
+        font-weight: bold;
+        border: 1px solid black;
+    }
+}
+</style>
